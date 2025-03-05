77af9d33205c12c19bbe9fd60d985468a0653b6c
Marcus Liam Nielsen <marcus.email2016@gmail.com>
2025-03-05 15:07:23 +0100

    Added some Arduino code
    - Added constructor to Mocktail class
    - Moved Mocktail class
    - Moves Outputs enum
    - Created mocktail array in Setup void

--- Mocktail_Machine/Mocktail_Machine.ino
+++ Mocktail_Machine/Mocktail_Machine.ino
@@ -9,6 +9,28 @@ int pin6 = 49;
 int pin7 = 51;
 int pin8 = 53;
 
+enum Outputs
+{
+  Test1,
+  Test2,
+  Test3,
+  Test4,
+  Test5,
+  Test6,
+  Test7,
+  Test8,
+};
+
+class Mocktail {
+  public:
+    String Name;
+    List<Outputs> Ingredients;
+    List<int> Ratios;
+
+    Mocktail(String name, List<Outputs> ingredients, List<int> ratios)
+      : Name(name), Ingredients(ingredients), Ratios(ratios) {}
+};
+
 void setup() {
   pinMode(pin1, OUTPUT); //sætter pin 39 til at være output
   pinMode(pin2, OUTPUT);
@@ -18,6 +40,12 @@ void setup() {
   pinMode(pin6, OUTPUT);
   pinMode(pin7, OUTPUT);
   pinMode(pin8, OUTPUT);
+  
+  Mocktail mocktails[3] = {
+    {"", {}, {}},
+    {"", {}, {}},
+    {"", {}, {}}
+  };
 }
 
 void loop() {
@@ -37,23 +65,6 @@ void loop() {
   delay(50);
   digitalWrite(pin4, LOW);
   delay(50);
+
+
 }
-
-enum Outputs
-{
-  Test1,
-  Test2,
-  Test3,
-  Test4,
-  Test5,
-  Test6,
-  Test7,
-  Test8,
-};
-
-class Mocktail {
-  public:
-    String Name;
-    List<Outputs> Ingredients;
-    List<int> Ratios;
-};

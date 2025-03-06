47cae2c5b9c2fa12136960ab2f59e40fd9ae29c8
Marcus Liam Nielsen <marcus.email2016@gmail.com>
2025-03-06 14:30:40 +0100

    Added more Arduino code

--- Mocktail_Machine/Mocktail_Machine.ino
+++ Mocktail_Machine/Mocktail_Machine.ino
@@ -1,3 +1,6 @@
+#include <Time.h>
+#include <TimeLib.h>
+
 #include <List.hpp>
 
 int pin1 = 39;
@@ -8,6 +11,9 @@ int pin5 = 47;
 int pin6 = 49;
 int pin7 = 51;
 int pin8 = 53;
+
+int button1 = 12;
+int button2 = 13;
 
 enum Outputs
 {
@@ -31,6 +37,12 @@ class Mocktail {
       : Name(name), Ingredients(ingredients), Ratios(ratios) {}
 };
 
+  Mocktail mocktails[3] = {
+    {"", {}, {}},
+    {"", {}, {}},
+    {"", {}, {}}
+  };
+
 void setup() {
   pinMode(pin1, OUTPUT); //sætter pin 39 til at være output
   pinMode(pin2, OUTPUT);
@@ -40,15 +52,49 @@ void setup() {
   pinMode(pin6, OUTPUT);
   pinMode(pin7, OUTPUT);
   pinMode(pin8, OUTPUT);
+}
+
+int flowspeed = 50;
+int cupAmount = 0;
+const int cupSize = 250;
+
+void pour(int index, int time)
+{
   
-  Mocktail mocktails[3] = {
-    {"", {}, {}},
-    {"", {}, {}},
-    {"", {}, {}}
+}
+
+void pourButton(int index)
+{
+  Mocktail mocktail = mocktails[i];
+
+  for (int i = 0; i < sizeof(mocktail.Ingredients) / sizeof(mocktails.Ingredients[0]); i++)
+  {
+    int index = 0;
+    for (int j = 0; j < 8; j++)
+    {
+      if (mocktail.Ingredients[i] == Outputs(j))
+      {
+        index = j;
+      }
+    }
+
+    int amount = cupSize / 100 * mocktail.Ratios[i];
+
+    pour(index, round(amount / flowspeed * 100));
   };
 }
 
 void loop() {
+  if (digitalRead(button1) == HIGH)
+  {
+    pourButton(0);
+  }
+  if (digitalRead(button2) == HIGH)
+  {
+    pourButton(1);
+  }
+
+  /*
   digitalWrite(pin1, HIGH);
   delay(50);
   digitalWrite(pin2, HIGH);
@@ -65,6 +111,5 @@ void loop() {
   delay(50);
   digitalWrite(pin4, LOW);
   delay(50);
-
-
+  */
 }
